10d968e4ec378b87340c93fa8f6cb8616e4b4962
KlaussVP <klauss.pinto@aluno.cefet-rj.br>
2021-01-19 18:13:44 -0300

    feat: header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <Router>
+        <Header />
         <Switch>
           <Route path='/' component={HomePage} />
         </Switch>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaShoppingCart } from 'react-icons/fa';
 
 export default function Header() {
 
     return (
-        <h1> Temporary header </h1>
+        <HeaderContainer>
+            <Logo>
+                <img src='/images/logo.png' alt='voltar à página principal'/>
+                <h1>MeditAí</h1>
+            </Logo>
+            <Icon>
+                <FaShoppingCart />
+                <p>Carrinho</p>
+            </Icon>
+        </HeaderContainer>
     );
 }
+
+const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background: #38B2B8;
+    color: #FFFFFF;
+    font-family: sans-serif;
+
+    h1 {
+        font-size: 22px;
+        font-weight: 400;
+        margin-left: 10px;
+    }
+`;
+
+const Logo = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 35px;
+
+    img {
+        height: 100%;
+        border-radius: 50%;
+    }
+`;
+
+const Icon = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 25px;
+
+    p {
+        font-size: 17px;
+        font-weight: 400;
+    }
+`;
